a4ca03498f15bd385aa89f94e5ca5a1ae9cb5dfc
Aboshxm2 <72500560+Aboshxm2@users.noreply.github.com>
2024-06-27 19:05:43 +0300

    first commit

---
+++ src/Aboshxm2/Switching/Main.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aboshxm2\Switching;
+
+use pocketmine\entity\Living;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener
+{
+    private array $entityTicks = [];
+
+    protected function onEnable(): void
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    /**
+     * @priority HIGHEST
+     */
+    public function onDamage(EntityDamageByEntityEvent $event): void
+    {
+        $living = $event->getEntity();
+        if(!$living instanceof Living) return;
+
+        $ticks = $living->ticksLived;
+        $cooldown = ($this->getConfig()->get('attack-cooldown', -1) >= 0) ? $this->getConfig()->get('attack-cooldown') : $event->getAttackCooldown();
+
+        if(
+            isset($this->entityTicks[$living->getId()]) &&
+            ($ticks - $this->entityTicks[$living->getId()]) < $cooldown
+        ) {
+            if($living->getLastDamageCause()->getBaseDamage() >= $event->getBaseDamage()) {                
+                $event->cancel();
+            }
+        }else {
+            $this->entityTicks[$living->getId()] = $ticks;
+        }
+
+        $event->setAttackCooldown(0);
+    }
+}
